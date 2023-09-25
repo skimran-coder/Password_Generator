b2d50da0c7c838f2d65407e4437a1476d7094c90
Sk. Imran Hussain <skimranpc@gmail.com>
2023-09-25 23:28:26 +0530

    Password generator project 95% complete, only need to fix shuffle

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const displayPassword = document.getElementById("password-display")
+const copyButton = document.getElementById("copy-btn")
+const copiedText = document.getElementById('copiedText')
 const lengthNumber = document.querySelector('#password-number');
 const inputSlider = document.querySelector('#slider');
 const upperCaseCheck = document.querySelector('#upperCase')
@@ -8,6 +11,7 @@ const allCheckBoxes = document.querySelectorAll('input[type=checkbox]')
 // console.log(allCheckBoxes);
 const strengthColor = document.querySelector('#display-strength');
 const symbols = "?/.,><;':[]{}=+-_()*&^%$#@!~`"
+const generateButton = document.querySelector('#submit-btn')
 
 // console.log(lengthNumber);
 
@@ -16,14 +20,17 @@ let checkCount = 0;
 handleSlider()
 
 function handleSlider(){
+    passwordLength = inputSlider.value
     lengthNumber.innerHTML = passwordLength;
-    inputSlider.value = passwordLength;
+    // console.log(passwordLength);
+    
     
 }
 
 inputSlider.addEventListener('change', function(e){
-    passwordLength = e.target.value
-    lengthNumber.innerHTML = passwordLength;
+    // passwordLength = e.target.value
+    // lengthNumber.innerHTML = passwordLength;
+    handleSlider();
 })
 
 function generateRndIntgr(min, max){
@@ -35,14 +42,14 @@ function generateRndNum(){
 }
 
 function generateLowerCase(){
-    return  generateRndIntgr(97,123)
+    return  String.fromCharCode(generateRndIntgr(97,122))
 }
 function generateUpperCase(){
-    return  generateRndIntgr(65,91)
+    return  String.fromCharCode(generateRndIntgr(65,90))
 }
 
 function generateSymbol(){
-    const randomIndex = generateRndIntgr(0, symbols.length)
+    const randomIndex = generateRndIntgr(0, symbols.length - 1)
     return symbols[randomIndex]
 }
 
@@ -62,7 +69,7 @@ function CalcStrength(){
     if (symbolsCheck.checked) hasSymbol = true;
     
 
-    if (hasUpperCase && hasLowerCase (hasNumber || hasSymbol) && passwordLength >= 8) {
+    if ((hasUpperCase && hasLowerCase ) && (hasNumber || hasSymbol) && (passwordLength >= 8)) {
         displayStrength("green")
     }
      else if ((hasUpperCase || hasLowerCase) && (hasNumber || hasSymbol) && passwordLength >= 6) {
@@ -74,15 +81,106 @@ function CalcStrength(){
 };
 
 
-// continue from here above codes are all good, no need to rewrite just read again
 
-allCheckBoxes.forEach(function (checkbox){
-    checkbox.addEventListener("change", function(){
-        checkCount = 0;
+
+
+allCheckBoxes.forEach(function (checkbox) {
+  checkbox.addEventListener("change", function () {     
+    if (checkbox.checked) {
+      checkCount++;
+    } else {
+      checkCount--;
+    }
+    console.log(checkCount);
+  });
+});
+
+const copyToClipboard = async function(){
+  
+  try {
+    await  navigator.clipboard.writeText(displayPassword.value)
+    copiedText.classList.remove('active');
+
+    setTimeout (function(){
+        copiedText.classList.add('active');
+    },2000)
+  } catch (error) {
+    console.log(`error occured`);
+  }
+} 
+
+copyButton.addEventListener('click', copyToClipboard);
+
+const generatePassword = () => {
+    let password = "";
+    displayPassword.value = "";
+    
+
+    if (upperCaseCheck.checked) {
+        password += generateUpperCase()
+    }
+
+    if (lowerCaseCheck.checked) {
+        password += generateLowerCase()
+
+    }
+    if (numbersCheck.checked) {
+        password += generateRndNum()
+    }
+    if (symbolsCheck.checked) {
+        password += generateSymbol()
+    }
+
+    console.log(password)
+
+    for (let i = checkCount; i < passwordLength; i++) {
+        let methodsArray = []
+
+        if (upperCaseCheck.checked) {
+            methodsArray.push(generateUpperCase)
+        }
+
+        if (lowerCaseCheck.checked){
+            methodsArray.push(generateLowerCase)
+        }
+
+        if (numbersCheck.checked){
+            methodsArray.push(generateRndNum)
+        }
+
+        if (symbolsCheck.checked){
+            methodsArray.push(generateSymbol)
+        }
+
+        const randomMethod = methodsArray[generateRndIntgr(0, methodsArray.length - 1)]
+
+        password += randomMethod();
         
-        
-    })
-})
+    }
+
+    console.log(password)
+
+    // shuffle
+
+    let passArray = Array.from(password)
+    console.log(passArray);
+    let newPass = "";
+    for (let i = 0; i < passArray.length; i++) {
+        newPass += (passArray[generateRndIntgr(0, passArray.length -1)])
+    }
+    console.log(newPass);
+
+    displayPassword.value = newPass;
+    CalcStrength();
+
+    // console.log(password);
+    // console.log(displayPassword);
+
+}
+
+generateButton.addEventListener('click', generatePassword)
+
+
 
 
 
