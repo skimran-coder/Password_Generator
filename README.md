b2689ccec66a8ed35c5e350e16be55c2b876d21e
Sk. Imran Hussain <skimranpc@gmail.com>
2023-09-28 02:34:49 +0530

    Added comments for better understanting and wrote README.md file.

--- README.md
+++ README.md
@@ -1,3 +1,81 @@
-# Password Generator Project
-<br>
-Author - Sk. Imran Hussain
+# Password Generator
+
+![Password Generator Screenshot](/Asset/passwordGenerator.png)
+
+A simple web-based tool to generate strong and secure passwords with various options.
+
+## Table of Contents
+
+- [Demo](#demo)
+- [Features](#features)
+- [Usage](#usage)
+- [Installation](#installation)
+- [How It Works](#how-it-works)
+- [Customization](#customization)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Demo
+
+You can try the live demo of this Password Generator [here](link-to-demo).
+
+## Features
+
+- Generate random passwords with specified length.
+- Customize your passwords by including uppercase letters, lowercase letters, numbers, and symbols.
+- Visual strength indicator for password security.
+- Copy generated passwords to the clipboard with one click.
+
+## Usage
+
+1. Visit the [Password Generator](link-to-demo) website.
+2. Adjust the password length using the slider.
+3. Choose the character types you want to include in your password.
+4. The strength indicator will display the password's security level.
+5. Click the "Generate Password" button.
+6. Your new password will appear in the input field.
+7. Click the copy button to copy the password to your clipboard.
+
+## Installation
+
+If you want to run this project locally, follow these steps:
+
+1. Clone this repository:
+
+   ```bash
+   git clone https://github.com/your-username/password-generator.git
+   ```
+
+2. Open the `index.html` file in your web browser.
+
+## How It Works
+
+The Password Generator uses JavaScript to generate random passwords based on your selected criteria. It utilizes the following character sets: uppercase letters, lowercase letters, numbers, and symbols. The generated password is then displayed and can be copied to the clipboard for easy use.
+
+## Customization
+
+You can customize the Password Generator by:
+
+- Adjusting the minimum and maximum password length in the JavaScript file (`index.js`).
+- Modifying the character sets used for generating passwords.
+- Changing the colors and styles of the user interface in the CSS file (`style.css`).
+
+Feel free to tailor the generator to your specific needs.
+
+## Contributing
+
+If you'd like to contribute to this project, please follow these guidelines:
+
+1. Fork the repository.
+2. Create a new branch for your feature: `git checkout -b feature-name`.
+3. Commit your changes: `git commit -m 'Add some feature'`.
+4. Push to your branch: `git push origin feature-name`.
+5. Submit a pull request.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+---
+
+Feel free to replace placeholders such as `link-to-demo` with the actual links and customize the content further to suit your project's details. This README.md provides a good starting point for documenting your Password Generator project.
--- index.js
+++ index.js
@@ -1,126 +1,119 @@
-const displayPassword = document.getElementById("password-display")
-const copyButton = document.getElementById("copy-btn")
-const copiedText = document.getElementById('copiedText')
+// DOM elements
+const displayPassword = document.getElementById("password-display");
+const copyButton = document.getElementById("copy-btn");
+const copiedText = document.getElementById('copiedText');
 const lengthNumber = document.querySelector('#password-number');
 const inputSlider = document.querySelector('#slider');
-const upperCaseCheck = document.querySelector('#upperCase')
-const lowerCaseCheck = document.querySelector('#lowerCase')
-const numbersCheck = document.querySelector('#numbers')
+const upperCaseCheck = document.querySelector('#upperCase');
+const lowerCaseCheck = document.querySelector('#lowerCase');
+const numbersCheck = document.querySelector('#numbers');
 const symbolsCheck = document.querySelector('#symbols');
-const allCheckBoxes = document.querySelectorAll('input[type=checkbox]')
-// console.log(allCheckBoxes);
+const allCheckBoxes = document.querySelectorAll('input[type=checkbox]');
 const strengthColor = document.querySelector('#display-strength');
-const symbols = "?/.,><;':[]{}=+-_()*&^%$#@!~`"
-const generateButton = document.querySelector('#submit-btn')
+const symbols = "?/.,><;':[]{}=+-_()*&^%$#@!~`";
+const generateButton = document.querySelector('#submit-btn');
 
-// console.log(lengthNumber);
+// Initialize password settings
+let passwordLength = 10; // Default password length
+let checkCount = 1; // Initially set to 1 because one checkbox is checked by default
+handleSlider(); // Initialize slider
+displayStrength("#ccc"); // Initialize strength indicator
 
-let passwordLength = 10;
-let checkCount = 1;
-handleSlider()
-displayStrength("#ccc")
-
-
-function handleSlider(){
-    passwordLength = inputSlider.value
+// Function to update password length from the slider
+function handleSlider() {
+    passwordLength = inputSlider.value;
     lengthNumber.innerHTML = passwordLength;
     const min = inputSlider.min;
     const max = inputSlider.max;
-    inputSlider.style.backgroundSize = ( (passwordLength - min)*100/(max - min)) + "% 100%"
-    // console.log(passwordLength);
-    
-    
+    inputSlider.style.backgroundSize = ((passwordLength - min) * 100 / (max - min)) + "% 100%";
 }
 
-inputSlider.addEventListener('input', function(){
-    // passwordLength = e.target.value
-    // lengthNumber.innerHTML = passwordLength;
+// Event listener for the slider
+inputSlider.addEventListener('input', function () {
     handleSlider();
-})
+});
 
-function generateRndIntgr(min, max){
-    return Math.round(Math.random() * (max-min)) + min;
+// Function to generate a random integer between min and max
+function generateRndIntgr(min, max) {
+    return Math.round(Math.random() * (max - min)) + min;
 }
 
-function generateRndNum(){
-    return generateRndIntgr(0,9)
+// Functions to generate random characters
+function generateRndNum() {
+    return generateRndIntgr(0, 9);
 }
 
-function generateLowerCase(){
-    return  String.fromCharCode(generateRndIntgr(97,122))
-}
-function generateUpperCase(){
-    return  String.fromCharCode(generateRndIntgr(65,90))
+function generateLowerCase() {
+    return String.fromCharCode(generateRndIntgr(97, 122));
 }
 
-function generateSymbol(){
-    const randomIndex = generateRndIntgr(0, symbols.length - 1)
-    return symbols[randomIndex]
+function generateUpperCase() {
+    return String.fromCharCode(generateRndIntgr(65, 90));
 }
 
-function displayStrength(color){
+function generateSymbol() {
+    const randomIndex = generateRndIntgr(0, symbols.length - 1);
+    return symbols[randomIndex];
+}
+
+// Function to display password strength color
+function displayStrength(color) {
     strengthColor.style.backgroundColor = color;
     strengthColor.style.boxShadow = `0px 0px 12px 1px ${color}`;
 }
 
-function CalcStrength(){
+// Function to calculate and display password strength
+function CalcStrength() {
     let hasUpperCase = false;
     let hasLowerCase = false;
     let hasNumber = false;
     let hasSymbol = false;
-    
+
     if (upperCaseCheck.checked) hasUpperCase = true;
     if (lowerCaseCheck.checked) hasLowerCase = true;
     if (numbersCheck.checked) hasNumber = true;
     if (symbolsCheck.checked) hasSymbol = true;
-    
 
-    if ((hasUpperCase && hasLowerCase ) && (hasNumber || hasSymbol) && (passwordLength >= 8)) {
-        displayStrength("#0f0")
-    }
-     else if ((hasUpperCase || hasLowerCase) && (hasNumber || hasSymbol) && passwordLength >= 6) {
-        displayStrength("#ff0")
-     }
-     else {
-        displayStrength("#f00")
-     }
-};
-
-
-
-
-
-allCheckBoxes.forEach(function (checkbox) {
-  checkbox.addEventListener("input", function () {     
-    if (checkbox.checked) {
-      checkCount++;
+    if ((hasUpperCase && hasLowerCase) && (hasNumber || hasSymbol) && (passwordLength >= 8)) {
+        displayStrength("#0f0"); // Strong password
+    } else if ((hasUpperCase || hasLowerCase) && (hasNumber || hasSymbol) && passwordLength >= 6) {
+        displayStrength("#ff0"); // Medium password
     } else {
-      checkCount--;
+        displayStrength("#f00"); // Weak password
     }
-    // console.log(checkCount);
-  });
+}
+
+// Event listeners for all checkboxes to count the checked ones
+allCheckBoxes.forEach(function (checkbox) {
+    checkbox.addEventListener("input", function () {
+        if (checkbox.checked) {
+            checkCount++;
+        } else {
+            checkCount--;
+        }
+    });
 });
 
-const copyToClipboard = async function(){
-  
-  try {
-    if (displayPassword.value != "") {
-        await  navigator.clipboard.writeText(displayPassword.value)
-    copiedText.classList.remove('active');
+// Function to copy the password to the clipboard
+const copyToClipboard = async function () {
+    try {
+        if (displayPassword.value != "") {
+            await navigator.clipboard.writeText(displayPassword.value)
+            copiedText.classList.remove('active');
 
-    setTimeout (function(){
-        copiedText.classList.add('active');
-    },2000)
-
+            setTimeout(function () {
+                copiedText.classList.add('active');
+            }, 2000)
+        }
+    } catch (error) {
+        console.log(`Error: ${error.message}`);
     }
-    
-  } catch (error) {
-    console.log(`Error: ${error.message}`);
-  }
-} 
+}
 
+// Event listener for the copy button
 copyButton.addEventListener('click', copyToClipboard);
 
+// Function to shuffle an array
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -128,7 +121,7 @@ function shuffleArray(array) {
     }
 }
 
-
+// Function to generate a password
 const generatePassword = () => {
     let password = "";
     displayPassword.value = "";
@@ -144,7 +137,6 @@ const generatePassword = () => {
         inputSlider.value = passwordLength;
         lengthNumber.innerHTML = passwordLength;
     }
-    
 
     if (upperCaseCheck.checked) {
         password += generateUpperCase()
@@ -152,7 +144,6 @@ const generatePassword = () => {
 
     if (lowerCaseCheck.checked) {
         password += generateLowerCase()
-
     }
     if (numbersCheck.checked) {
         password += generateRndNum()
@@ -168,43 +159,31 @@ const generatePassword = () => {
             methodsArray.push(generateUpperCase)
         }
 
-        if (lowerCaseCheck.checked){
+        if (lowerCaseCheck.checked) {
             methodsArray.push(generateLowerCase)
         }
 
-        if (numbersCheck.checked){
+        if (numbersCheck.checked) {
             methodsArray.push(generateRndNum)
         }
 
-        if (symbolsCheck.checked){
+        if (symbolsCheck.checked) {
             methodsArray.push(generateSymbol)
         }
 
         const randomMethod = methodsArray[generateRndIntgr(0, methodsArray.length - 1)]
 
         password += randomMethod();
-        
     }
 
-
     // shuffle
-
     const passwordArray = Array.from(password);
     shuffleArray(passwordArray);
     const shuffledPassword = passwordArray.join('');
 
     displayPassword.value = shuffledPassword;
     CalcStrength();
-
-    // console.log(password);
-    // console.log(displayPassword);
-
 }
 
-generateButton.addEventListener('click', generatePassword)
-
-
-
-
-
-
+// Event listener for the password generation button
+generateButton.addEventListener('click', generatePassword);
